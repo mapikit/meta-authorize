abf428b5f26a7657656d6e6038d5f52cb53e63a4
Fábio Meneses Junior <flgmjr@gmail.com>
2022-05-24 09:52:10 -0300

    start

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+// This is supposed to be a package containing these functions,
+// Which should all be independent from eachother
+
+// Create Token
+// > Receiving random info
+// > Receiving an Auth key
+// >> Outputs the Token
+
+// Verify token
+// > with an Auth Key
+// >> Outputs a boolean
+
+// Hash Pass
+// >> Returns a string
+
+// Verify Against Hash
+// >> Returns boolean
